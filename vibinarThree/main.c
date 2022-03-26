8c46b652eaadedbeb38a49a0a5276f4e699f071b
JoraVer <tvbsf2001@gmail.com>
2022-03-26 11:19:57 +0200

    Assignment for the second part of the third lesson

--- vibinarThree/main.c
+++ vibinarThree/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n)
+{
+    if (n > 1)
+    {
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+    else
+        return false;
+}
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    int n;
+    char ans = 'N';
+    printf("Input a number: ");
+    scanf_s("%d", &n);
+    if (isPrime(n))
+        ans = 'Y';
+    printf("Is prime: %d\n? ", ans);
 }
